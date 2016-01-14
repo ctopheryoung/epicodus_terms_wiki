3aef84db14b27836560bdad2fd299fbd7ef52a14
Chris Young and Ryan King <ctopheryoung@gmail.com>
2016-01-14 14:46:35 -0800

    cloned nav to all pages with active class, finished all term html pages

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     var response = $("input#response").val();
     $(".response").text(response);
-
     event.preventDefault();
-
   });
 });
